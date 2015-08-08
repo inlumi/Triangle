7be367d27df425f4b6e8e06ee10df4174676ba66
Maria <marie.valerievna@gmail.com>
2015-08-08 23:45:31 +0300

    First real commit

---
+++ TriangleTests/UnitTest1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpcTriangle;
+
+namespace TriangleTests
+{
+    [TestClass]
+    public class TriangleUnitTest
+    {
+        /// <summary>
+        /// Пробует создать несуществующий треугольник (одна из сторон больше суммы двух других)
+        /// </summary>
+        [TestMethod]
+        public void CreateUnexistingTriangle()
+        {
+            // Arrange
+            float Side1 = 1.0F;
+            float Side2 = 1.0F;
+            float Side3 = 2.0F;
+            bool expected = false;
+            bool actual = true;
+
+            // Act
+            try
+            {
+                Triangle UnexistTriangle = TriangleFactory.CreateTriangle(Side1, Side2, Side3);
+            }
+            catch(System.ArgumentException)
+            {
+                actual = false;
+            }
+
+            //  Assert
+            Assert.AreEqual(expected, actual);
+        }
+        /// <summary>
+        /// Проверяет правильность подсчёта площади обычного треугольника.
+        /// </summary>
+        [TestMethod]
+        public void TryCountSimpleSpace()
+        {
+            // Arrange
+            float Side1 = 4.0F;
+            float Side2 = 5.0F;
+            float Side3 = 6.5F;
+            double _expected = 9.99511599482467;
+
+            //Act
+            Triangle SimpleTriangle = TriangleFactory.CreateTriangle(Side1, Side2, Side3);
+            double actual = SimpleTriangle.CountSpace();
+
+            //Assert
+            Assert.AreEqual((Math.Abs(_expected - actual) < 0.1), true);
+        }
+        /// <summary>
+        /// Пробует создать треугольник с существующими сторонами, не являющийся правильным.
+        /// Проверяет, является ли созданный треугольник инстансом класса простого, не правильного треугольника.
+        /// Так же создаёт правильный треугольник и проверяет, является ли он объектом класса правильного треугольника.
+        /// </summary>
+        [TestMethod]
+        public void CreateRightTriangle()
+        {
+            // Arrange
+            float Side1 = 4.1F;
+            float Side2 = 5.0F;
+            float Side3 = 6.5F;
+            float Cath1 = 3.0F;
+            float Cath2 = 4.0F;
+            float Hypo = 5.0F;
+
+            // Act
+            Triangle rightTriangle = TriangleFactory.CreateTriangle(Cath1, Cath2, Hypo);
+            Triangle notRightTriangle = TriangleFactory.CreateTriangle(Side1, Side2, Side3);
+
+            //  Assert
+            Assert.AreEqual(rightTriangle is RightTriangle, true);
+            Assert.AreEqual(notRightTriangle is RightTriangle, false);
+        }
+        /// <summary>
+        /// Проверяет правильность подсчёта площади правильного треугольника.
+        /// </summary>
+        [TestMethod]
+        public void TryCountRightSpace()
+        {
+            // Arrange
+            float Side1 = 3.0F;
+            float Side2 = 4.0F;
+            float Side3 = 5.0F;
+            double _expected = 6.0;
+
+            //Act
+            Triangle SimpleTriangle = TriangleFactory.CreateTriangle(Side1, Side2, Side3);
+            double actual = SimpleTriangle.CountSpace();
+
+            //Assert
+            Assert.AreEqual((Math.Abs(_expected - actual) < 0.1), true);
+        }
+    }
+}
---
+++ triangle/Triangle.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpcTriangle
+{
+    /// <summary>
+    /// Базовый класс, содержащий методы для создания, определения и вычисления площади треугольника.
+    /// Для подсчёта площади необходимо вызвать функцию CountSpace с нужными сторонами в качестве аргументов.
+    /// </summary>
+    public class Triangle
+    {
+        protected float Side1;
+        protected float Side2;
+        protected float Side3;
+
+        public Triangle(float Side1, float Side2, float Side3)
+        {
+            this.Side1 = Side1;
+            this.Side2 = Side2;
+            this.Side3 = Side3;
+        }
+
+        /// <summary>
+        /// Метод, проверяющий треугольник на существование (каждая сторона должна быть меньше суммы двух других сторон).
+        /// </summary>
+        /// <param name="Side1">Одна из сторон треугольника</param>
+        /// <param name="Side2">Другая сторона треугольника</param>
+        /// <param name="Side3">Оставшаяся сторона треугольника</param>
+        /// <returns></returns>
+        public static bool IfTriangle(double Side1, double Side2, double Side3)
+        {
+            if (Side1 > 0 & Side2 > 0 & Side3 > 0)
+            {
+                if ((Side1 + Side2 > Side3) & (Side1 + Side3 > Side2) & (Side2 + Side3 > Side1))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// Метод, вычисляющий периметр треугольника.
+        /// </summary>
+        public double SemiPerimeter
+        {
+            get
+            {
+                return (this.Side1 + this.Side2 + this.Side3) / 2;
+            }
+        }
+        /// <summary>
+        /// Метод, вычисляющий площадь любого треугольника с целочисленными сторонами.
+        /// </summary>
+        /// <returns></returns>
+        public virtual double CountSpace()
+        {
+            return Math.Sqrt(this.SemiPerimeter *
+                        (this.SemiPerimeter - this.Side1) *
+                        (this.SemiPerimeter - this.Side2) *
+                        (this.SemiPerimeter - this.Side3));
+        }
+    }
+    /// <summary>
+    /// Класс, содержащий методы для создания, определения и вычисления площади прямоугольного треугольника.
+    /// Для подсчёта площади необходимо вызвать функцию CountSpace с нужными сторонами в качестве аргументов.
+    /// </summary>
+    public class RightTriangle : Triangle
+    {
+        public RightTriangle(float Сathetus1, float Сathetus2, float Hypotenuze) : base(Сathetus1, Сathetus2, Hypotenuze)
+        {
+        }
+
+        /// <summary>
+        /// Метод, вычисляющий площадь прямоугольного треугольника, основываясь на его нецелочисленных катетах.
+        /// </summary>
+        /// <returns>Space</returns>
+        public override double CountSpace()
+        {
+            return (1.0 / 2.0) * this.Side1 * this.Side2;
+        }
+    }
+    /// <summary>
+    /// Фабрика для создания экземпляров треугольника.
+    /// </summary>
+    public class TriangleFactory
+    {
+        public static Triangle CreateTriangle(float Side1, float Side2, float Side3)
+        {
+            if (Triangle.IfTriangle(Side1, Side2, Side3) == true)
+            {
+                float Hypotenuze = Math.Max(Math.Max(Side1, Side2), Side3);
+                float Cathetus1 = Math.Min(Math.Min(Side1, Side2), Side3);
+                float Cathetus2 = (Side1 + Side2 + Side3 - Hypotenuze - Cathetus1);
+
+                if (Math.Pow(Cathetus1, 2) + Math.Pow(Cathetus2, 2) == Math.Pow(Hypotenuze, 2))
+                {
+                    return new RightTriangle(Cathetus1, Cathetus2, Hypotenuze);
+                }
+                else
+                {
+                    return new Triangle(Cathetus1, Cathetus2, Hypotenuze);
+                }
+            }
+            else
+            {
+                throw new System.ArgumentException("Not a triangle!");
+            }
+        }
+    }
+}
